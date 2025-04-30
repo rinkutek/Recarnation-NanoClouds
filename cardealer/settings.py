bc8897a93d04dcec35c120f85b96dd1dbffee93b
ApoorvaShastry10 <1998shastry@gmail.com>
2025-04-30 12:30:30 -0700

    Update settings.py

--- cardealer/settings.py
+++ cardealer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '934nw3r62@!m0^ksgw3#31tntglnr%td+-_b89xpu2@q2zqv=d'
 DEBUG = True
 
 # ALLOWED_HOSTS = ['floating-badlands-41165.herokuapp.com', 'cardealerapp.co', 'www.cardealerapp.co', '127.0.0.1:8000']
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'recarnation-env.eba-fj3pap7q.us-east-1.elasticbeanstalk.com', 'trial-1988947.okta.com' ]
 
 LOGIN_REDIRECT_URL = '/dashboard/'
 
